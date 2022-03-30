ec2385d26c4f189710621a3c60206d329f018214
Andrew Dunleavy <andrew.dunleavy@thebeansgroup.com>
2022-03-30 16:24:09 +0100

    notes route

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 const PORT = 3000
-
+const noteRoutes = require('./noteRoute')
 
 
 app.use(express.static('public'))
@@ -12,7 +12,7 @@ app.listen(PORT, () => {
 })
 
 const sequelize = require("./db.js");
-const Note = require('./notes.js');
+
 
 async function main() {
 
@@ -26,36 +26,7 @@ async function main() {
         console.error('Unable to connect to the database:', error);
     }
 }
-app.post('/notes', async (req, res) => {
-    await Note.create(req.body)
-    res.send("note has beeen added")
-})
-
-
-app.get('/notes', async (req, res) => {
-    const notes = await Note.findAll();
-    res.send(notes);
-})
-
-app.get('/notes/:id', async (req, res) => {
-    const requestedId = req.params.id
-    const note = await Note.findOne({ where: { id: requestedId } })
-    res.send(note)
-})
-
-app.put('/notes/:id', async (req, res) => {
-    const requestedId = req.params.id
-    const note = await Note.findOne({ where: { id: requestedId } })
-    note.name = req.body.name
-    note.info = req.body.info
-    await note.Save()
-    res.send("note updated")
-})
-app.delete('/notes/:id', async (req, res) => {
-    const requestedId = req.params.id
-    await Note.destroy({where : {id : requestedId}})
-    res.send("note deleted")
-})
+app.use('/notes', noteRoutes)
 
 main()
 
--- noteRoute.js
+++ noteRoute.js
@@ -1,4 +1,39 @@
 const express = require('express')
 const router = express.Router()
+const Note = require('./notes.js');
 
 router.route('/')
+
+.post( async (req, res) => {
+    await Note.create(req.body)
+    res.send("note has beeen added")
+})
+
+
+.get( async (req, res) => {
+    const notes = await Note.findAll();
+    res.send(notes);
+})
+
+router.route('/:id')
+.get( async (req, res) => {
+    const requestedId = req.params.id
+    const note = await Note.findOne({ where: { id: requestedId } })
+    res.send(note)
+})
+
+.put( async (req, res) => {
+    const requestedId = req.params.id
+    const note = await Note.findOne({ where: { id: requestedId } })
+    note.name = req.body.name
+    note.info = req.body.info
+    await note.save()
+    res.send("note updated")
+})
+.delete( async (req, res) => {
+    const requestedId = req.params.id
+    await Note.destroy({where : {id : requestedId}})
+    res.send("note deleted")
+})
+
+module.exports = router

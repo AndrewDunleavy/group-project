7ea9a152a8f29ba40258ed9493a700dd4893346f
Andrew Dunleavy <andrew.dunleavy@thebeansgroup.com>
2022-03-31 14:39:02 +0100

    heroku login

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const PORT = 3000
 const noteRoutes = require('./noteRoute')
 const cors = require('cors')
 
+
 app.use(cors())
 app.use(express.static('public'))
 app.use(express.urlencoded({ extended: true }))
@@ -20,13 +21,11 @@ async function main() {
     await sequelize.sync()
     console.log("db is ready")
 
-    try {
+
         await sequelize.authenticate();
         console.log('Connection has been established successfully.');
-    } catch (error) {
-        console.error('Unable to connect to the database:', error);
-    }
-}
+    } 
+
 app.use('/notes', noteRoutes)
 
 main()

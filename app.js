6408be0271cb659140c9774ae7f2e2f91ae21221
Deniz <deniz.gulsin@thebeansgroup.com>
2022-03-31 13:23:21 +0100

    setup cors

--- app.js
+++ app.js
@@ -1,32 +1,30 @@
-const express = require('express')
-const app = express()
-const PORT = 3000
-const noteRoutes = require('./noteRoute')
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const PORT = 3000;
+const noteRoutes = require("./noteRoute");
 
-
-app.use(express.static('public'))
-app.use(express.urlencoded({ extended: true }))
-app.use(express.json())
+app.use(cors());
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use("/notes", noteRoutes);
 app.listen(PORT, () => {
-    console.log(`app running on port ${PORT}`)
-})
+  console.log(`app running on port ${PORT}`);
+});
 
 const sequelize = require("./db.js");
 
-
 async function main() {
+  await sequelize.sync();
+  console.log("db is ready");
 
-    await sequelize.sync()
-    console.log("db is ready")
-
-    try {
-        await sequelize.authenticate();
-        console.log('Connection has been established successfully.');
-    } catch (error) {
-        console.error('Unable to connect to the database:', error);
-    }
+  try {
+    await sequelize.authenticate();
+    console.log("Connection has been established successfully.");
+  } catch (error) {
+    console.error("Unable to connect to the database:", error);
+  }
 }
-app.use('/notes', noteRoutes)
 
-main()
-
+main();
